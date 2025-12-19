e74e6d2b211d4512bf5b4385d03c4826e1139898
Gabri180 <ys7b4rgtjc@privaterelay.appleid.com>
2025-12-19 19:53:20 +0100

    Arreglado astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://LukyStryky.github.io',
   base: '/Portafolio',
+  trailingSlash: 'always',
+  build: {
+    assets: '_astro'
+  }
 });
